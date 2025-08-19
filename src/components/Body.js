8c2f8b8060cb6d39e3201495739314c7bc071cfc
Prachi Takke <takkeprachi15@gmail.com>
2025-08-19 16:53:54 +0530

    signIn/signUp layout

---
+++ src/components/Body.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../components/Login";
+import Browse from "../components/Browse";
+import {RouterProvider} from "react-router-dom";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "/browse", element: <Browse /> },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
